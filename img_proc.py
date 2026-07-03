26304d5eaec1055c4c6492a7dacabd375f3cfffd
KaranChawlaD <karan.chawlad@gmail.com>
2026-07-02 23:40:39 -0400

    feat: skeletonization result storing

--- img_proc.py
+++ img_proc.py
@@ -2,7 +2,9 @@ import cv2
 from skimage.morphology import skeletonize
 import numpy as np
 
-img = cv2.imread("./test_images/lebron.png", cv2.IMREAD_GRAYSCALE)
+path = "circle.png"
+
+img = cv2.imread(f"./test_images/{path}", cv2.IMREAD_GRAYSCALE)
 
 if img is None:
     raise ValueError("could not load image at path")
@@ -16,5 +18,7 @@ skeleton = skeletonize(bool_img)
 cv2.namedWindow('cv2 output', cv2.WINDOW_KEEPRATIO)
 
 cv2.imshow('cv2 output', (skeleton * 255).astype(np.uint8))
+
+cv2.imwrite(f"./output_images/{path}", (skeleton * 255).astype(np.uint8))
 
 cv2.waitKey(0)
