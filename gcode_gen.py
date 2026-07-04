dc1b3bea8b3fd743208649150754b6c34d783b90
KaranChawlaD <karan.chawlad@gmail.com>
2026-07-03 21:46:43 -0400

    feat: initial logic for gcode gen

--- gcode_gen.py
+++ gcode_gen.py
@@ -4,12 +4,20 @@ def lift():
 def drop():
     return "G0 Z0"
 
-def create_command_chain(matrix):
+def create_command_chain(matrix, one_neighbors):
 
     cmds = []
 
     # auto home initially
     cmds.append("G28")
+
+    for pt in one_neighbors:
+        cmds.append(lift)
+        cmds.append(f"G0 X{pt[0]} Y{pt[1]}")
+        cmds.append(drop)
+
+        # now go to the pt's neighbour, if it exists (while loop)
+        # pop pt if it exists in one_neighbors
 
 if __name__ == "__main__":
     pass
